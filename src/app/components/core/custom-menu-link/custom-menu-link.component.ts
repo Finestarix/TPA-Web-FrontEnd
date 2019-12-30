75cf7d49ac76c38c84d47563202e986f68ef3df2
finestarix <irenaldyleonarto@gmail.com>
2019-12-31 00:30:58 +0700

    Reformat Code, Connect to Database with Param Bug

---
+++ src/app/components/core/custom-menu-link/custom-menu-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-menu-link',
+  templateUrl: './custom-menu-link.component.html',
+  styleUrls: ['./custom-menu-link.component.scss']
+})
+export class CustomMenuLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
